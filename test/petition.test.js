3670929756fe1af7b047ff273d17378ebf5b3081
edsadr <edsadr@gmail.com>
2015-01-31 16:53:41 +0000

    Refactoring tests

--- test/petition.test.js
+++ test/petition.test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var nocker = require('./tools/nocker.js');
-var expect = require('chai').expect;
 var testsInfo = require('./tests');
 
 describe('change-api petition endpoint', function () {
@@ -11,60 +10,17 @@ describe('change-api petition endpoint', function () {
     return index.resource === 'petitions';
   });
 
-  it('Getting petitions ID by URL should throw an error for URL missing', function () {
-    expect(function () {
-      client.petitions.getIdByUrl();
-    }).to.throw('a petition url is required');
-  });
-
-  it('Getting petitions ID by URL should throw an error for callback missing', function () {
-    expect(function () {
-      client.petitions.getIdByUrl('fakeUrl');
-    }).to.throw('a callback is required');
-  });
-
-  it('Getting petitions by ID should throw an error for id missing', function () {
-    expect(function () {
-      client.petitions.getByID();
-    }).to.throw('a petition id is required');
-  });
-
-  it('Getting petitions by ID should throw an error for callback missing', function () {
-    expect(function () {
-      client.petitions.getByID('fakeID');
-    }).to.throw('a callback is required');
-  });
-
-  it('Getting petitions by URL should throw an error for URL missing', function () {
-    expect(function () {
-      client.petitions.getByUrl();
-    }).to.throw('a petition url is required');
-  });
-
-  it('Getting petitions by URL should throw an error for callback missing', function () {
-    expect(function () {
-      client.petitions.getByUrl('fakeUrl');
-    }).to.throw('a callback is required');
-  });
-
-  var createTest = function(client, resource, ctest) {    
-    client[resource][ctest.method](ctest.args, function (err, res, body) {    
-      if (err) {  
-        console.log(body);
-        throw err;        
-      }      
-      describe('change-api petition endpoint method '+ctest.method, function () {
-        it(ctest.behavior,function (){        
-          expect(res.statusCode).to.be.equal(ctest.status);          
-        });
-      });
-    });
-  };
-
+  var methods = ['getIdByUrl','getByID','getByUrl'];
+    
+  //Test parameters and callbacks missing
+  for (var i = 0; i < methods.length; i++) {
+    nocker.testRequirements(client, resourceTests[0].resource, methods[i]);
+  }
+    
   //Mocking each API resource calls and creating the test
   for (var k = 0; k < resourceTests[0].cases.length; k++) {    
     var ctest = resourceTests[0].cases[k];            
     nocker.loadCall(ctest.url, ctest.verb, ctest.name, ctest.params);
-    createTest(client, resourceTests[0].resource, ctest);    
+    nocker.createTest(client, resourceTests[0].resource, ctest);    
   }
 });
--- test/user.test.js
+++ test/user.test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var nocker = require('./tools/nocker.js');
-var expect = require('chai').expect;
 var testsInfo = require('./tests');
 
 describe('change-api user endpoint', function () {
@@ -11,48 +10,17 @@ describe('change-api user endpoint', function () {
     return index.resource === 'users';
   });
 
-  it('Getting users ID by URL should throw an error for URL missing', function () {
-    expect(function () {
-      client.users.getIdByUrl();
-    }).to.throw('a user url is required');
-  });
-
-  it('Getting users ID by URL should throw an error for callback missing', function () {
-    expect(function () {
-      client.users.getIdByUrl('fakeUrl');
-    }).to.throw('a callback is required');
-  });
-
-  it('Getting users by ID should throw an error for id missing', function () {
-    expect(function () {
-      client.users.getByID();
-    }).to.throw('a user id is required');
-  });
-
-  it('Getting users by ID should throw an error for callback missing', function () {
-    expect(function () {
-      client.users.getByID('fakeID');
-    }).to.throw('a callback is required');
-  });
-
-  var createTest = function(client, resource, ctest) {    
-    client[resource][ctest.method](ctest.args, function (err, res, body) {    
-      if (err) {  
-        console.log(body);
-        throw err;        
-      }      
-      describe('change-api user endpoint method '+ctest.method, function () {
-        it(ctest.behavior,function (){        
-          expect(res.statusCode).to.be.equal(ctest.status);          
-        });
-      });
-    });
-  };
-
+  var methods = ['getIdByUrl','getByID','getPetitions', 'getSignatures'];
+  
+  //Test parameters and callbacks missing
+  for (var i = 0; i < methods.length; i++) {
+    nocker.testRequirements(client, resourceTests[0].resource, methods[i]);
+  }
+  
   //Mocking each API resource calls and creating the test
   for (var k = 0; k < resourceTests[0].cases.length; k++) {    
     var ctest = resourceTests[0].cases[k];            
     nocker.loadCall(ctest.url, ctest.verb, ctest.name, ctest.params);
-    createTest(client, resourceTests[0].resource, ctest);    
+    nocker.createTest(client, resourceTests[0].resource, ctest);    
   }
 });
